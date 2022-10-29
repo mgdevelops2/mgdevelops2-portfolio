87e3645a9d933bde7bdad839f449c285eda7d880
Mike Gonzalez <Mike.Gon1217@gmail.com>
2022-10-29 17:03:47 -0400

    adding bootstrap

--- src/index.js
+++ src/index.js
@@ -2,22 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import NavBar from './Components/NavBar';
-import Main from './Components/Main';
-import Showcase from './Components/Showcase';
-import Footer from './Components/Footer';
-import reportWebVitals from './reportWebVitals';
+// import Main from './Components/Main';
+// import Showcase from './Components/Showcase';
+// import Footer from './Components/Footer';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <NavBar />
-    <Main />
-    <Showcase />
-    <Footer />
+    {/* <Main /> */}
+    {/* <Showcase /> */}
+    {/* <Footer /> */}
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
