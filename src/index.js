8d0f2d30f25589209ac3f368c4ac3dddb6c446d1
Mike Gonzalez <Mike.Gon1217@gmail.com>
2022-10-29 16:21:37 -0400

    updated index file and added components folder for basic structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import NavBar from './Components/NavBar';
+import Main from './Components/Main';
+import Showcase from './Components/Showcase';
+import Footer from './Components/Footer';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar />
+    <Main />
+    <Showcase />
+    <Footer />
   </React.StrictMode>
 );
 
